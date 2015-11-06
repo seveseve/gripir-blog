48ecc14671c989503e103d42588c408ab2c057e4
Severin Friede <sevefriede@gmail.com>
2015-11-06 20:38:17 +0100

    Changed navigation bar

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -3,7 +3,7 @@
 angular.module('gripirBlogApp')
   .controller('NavbarCtrl', function ($scope, $location, Auth) {
     $scope.menu = [{
-      'title': 'Home',
+      'title': 'Blog',
       'link': '/'
     }];
 
