e37d8dffc83615b1679536073d2046fae042f28b
Severin <sevefriede@gmail.com>
2016-01-04 22:48:27 +0100

    #1 create activities and pagination added
    It is possible to create new posts. Pagination from ui.bootstrap added

--- client/app/admin/blog-mgmt/blog-mgmt.controller.js
+++ client/app/admin/blog-mgmt/blog-mgmt.controller.js
@@ -2,23 +2,35 @@
 
 angular.module('gripirBlogApp')
   .controller('BlogMgmtCtrl', function ($scope, $http, Modal, socket, notify) {
+
     $scope.activities = [];
+    $scope.creating = false;
 
-    $http.get('/api/activities').success(function(activities) {
-      $scope.activities = activities;
-      socket.syncUpdates('activity', $scope.activities);
-    });
+    $scope.filteredActivities = [];
+    $scope.currentPage = 1;
+    $scope.numPerPage = 5;
+    $scope.maxSize = 3;
 
-    $scope.addActivity = function() {
-      if($scope.newActivity === '') {
-        return;
-      }
+    var updateActivities = function() {
+      $http.get('/api/activities')
+        .success(function(activities) {
+          $scope.activities = activities;
+          socket.syncUpdates('activity', $scope.activities);
+        
+          var begin = (($scope.currentPage - 1) * $scope.numPerPage);
+          var end = begin + $scope.numPerPage;
+          $scope.filteredActivities = $scope.activities.slice(begin, end);
+        });
+    };
 
-      $http.post('/api/activities', { name: $scope.newActivity });
-      $scope.newActivity = '';
+    updateActivities();
+
+    $scope.pageChanged = function() {
+      updateActivities();
     };
 
     $scope.selectActivity = function(activity) {
+      $scope.creating = false;
       $scope.selectedActivity = {
         id: activity._id,
         text: activity.text,
@@ -28,7 +40,25 @@ angular.module('gripirBlogApp')
     };
 
     $scope.unselect = function() {
+      $scope.creating = false;
       $scope.selectedActivity = null;
+    };
+
+    $scope.showCreateForm = function () {
+      $scope.creating = true;
+    };
+
+    $scope.createNew = function () {
+      $http.post('/api/activities', {
+        text: $scope.selectedActivity.text,
+        title: $scope.selectedActivity.title,
+        author: $scope.selectedActivity.author
+      }).success(function () {
+        notify({ message: 'Created!', duration: 2000, classes: ["alert-success"] });
+        $scope.creating = false;
+        $scope.selectedActivity = null;
+        updateActivities();
+      });
     };
 
     $scope.updateSelected = function() {
@@ -36,14 +66,19 @@ angular.module('gripirBlogApp')
         text: $scope.selectedActivity.text,
         title: $scope.selectedActivity.title,
         author: $scope.selectedActivity.author
+      }).success(function () {
+        notify({ message: 'Updated!', duration: 2000, classes: ["alert-success"] });
+        updateActivities();
       });
-      notify({ message: 'Updated!', duration: 2000, classes: ["alert-success"] });
     };
 
     $scope.deleteActivity = Modal.confirm.delete(function() {
-      $http.delete('/api/activities/' + $scope.selectedActivity.id);
-      notify({ message: 'Removed!', duration: 2000, classes: ["alert-success"] });
-      $scope.unselect();
+      $http.delete('/api/activities/' + $scope.selectedActivity.id)
+      .success(function () {
+        notify({ message: 'Removed!', duration: 2000, classes: ["alert-success"] });
+        $scope.unselect();
+        updateActivities();
+      }); 
     });
 
     $scope.$on('$destroy', function () {
